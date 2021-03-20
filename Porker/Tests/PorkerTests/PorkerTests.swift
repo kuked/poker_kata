beb1563b359654fd5c68fab524c9cd350405f19c
kuked <kedoku@gmail.com>
2021-03-20 15:45:54 +0900

    Rename helper methods.

--- Porker/Tests/PorkerTests/PorkerTests.swift
+++ Porker/Tests/PorkerTests/PorkerTests.swift
@@ -16,12 +16,12 @@ final class PorkerTests: XCTestCase {
         var card1: Card
         var card2: Card
 
-        card1 = fromString("A❤︎")
-        card2 = fromString("2❤︎")
+        card1 = cardFrom("A❤︎")
+        card2 = cardFrom("2❤︎")
         XCTAssertTrue(card1.hasSameSuit(card2))
 
-        card1 = fromString("A♠︎")
-        card2 = fromString("2❤︎")
+        card1 = cardFrom("A♠︎")
+        card2 = cardFrom("2❤︎")
         XCTAssertFalse(card1.hasSameSuit(card2))
     }
 
@@ -29,98 +29,98 @@ final class PorkerTests: XCTestCase {
         var card1: Card
         var card2: Card
 
-        card1 = fromString("A♠︎")
-        card2 = fromString("A❤︎")
+        card1 = cardFrom("A♠︎")
+        card2 = cardFrom("A❤︎")
         XCTAssertTrue(card1.hasSameRank(card2))
 
-        card1 = fromString("A♠︎")
-        card2 = fromString("2❤︎")
+        card1 = cardFrom("A♠︎")
+        card2 = cardFrom("2❤︎")
         XCTAssertFalse(card1.hasSameRank(card2))
     }
 
     func testCardEqual() {
-        XCTAssertEqual(fromString("A♠︎"), fromString("A♠︎"))
+        XCTAssertEqual(cardFrom("A♠︎"), cardFrom("A♠︎"))
 
-        XCTAssertNotEqual(fromString("A♠︎"), fromString("2♠︎"))
-        XCTAssertNotEqual(fromString("A♠︎"), fromString("A❤︎"))
-        XCTAssertNotEqual(fromString("A♠︎"), fromString("Q❤︎"))
+        XCTAssertNotEqual(cardFrom("A♠︎"), cardFrom("2♠︎"))
+        XCTAssertNotEqual(cardFrom("A♠︎"), cardFrom("A❤︎"))
+        XCTAssertNotEqual(cardFrom("A♠︎"), cardFrom("Q❤︎"))
     }
 
     func testHandNotation() {
         var hand: Hand
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertEqual(hand.notation, "straight flush: A❤︎ K❤︎")
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("3❤︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("3❤︎")])
         XCTAssertEqual(hand.notation, "flush: A❤︎ 3❤︎")
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("K♠︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("K♠︎")])
         XCTAssertEqual(hand.notation, "straight: A❤︎ K♠︎")
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("A♠︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("A♠︎")])
         XCTAssertEqual(hand.notation, "one pair: A❤︎ A♠︎")
 
-        hand = Hand(cards: [fromString("J❤︎"), fromString("K♠︎")])
+        hand = Hand(cards: [cardFrom("J❤︎"), cardFrom("K♠︎")])
         XCTAssertEqual(hand.notation, "high card: J❤︎ K♠︎")
     }
 
     func testIsPair() {
         var hand: Hand
 
-        hand = handFromString("one pair: A❤︎ A♠︎")
+        hand = handFrom("one pair: A❤︎ A♠︎")
         XCTAssertTrue(hand.isPair)
         
-        hand = handFromString("high card: J❤︎ K♠︎")
+        hand = handFrom("high card: J❤︎ K♠︎")
         XCTAssertFalse(hand.isPair)
     }
 
     func testIsFlush() {
         var hand: Hand
 
-        hand = Hand(cards: [fromString("J❤︎"), fromString("K❤︎")])
+        hand = Hand(cards: [cardFrom("J❤︎"), cardFrom("K❤︎")])
         XCTAssertTrue(hand.isFlush)
         
-        hand = Hand(cards: [fromString("J❤︎"), fromString("K♠︎")])
+        hand = Hand(cards: [cardFrom("J❤︎"), cardFrom("K♠︎")])
         XCTAssertFalse(hand.isFlush)
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertFalse(hand.isFlush)
     }
 
     func testIsHighCard() {
         var hand: Hand
 
-        hand = Hand(cards: [fromString("J❤︎"), fromString("K♠︎")])
+        hand = Hand(cards: [cardFrom("J❤︎"), cardFrom("K♠︎")])
         XCTAssertTrue(hand.isHighCard)
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("A♠︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("A♠︎")])
         XCTAssertFalse(hand.isHighCard)
 
-        hand = Hand(cards: [fromString("J❤︎"), fromString("K❤︎")])
+        hand = Hand(cards: [cardFrom("J❤︎"), cardFrom("K❤︎")])
         XCTAssertFalse(hand.isHighCard)
     }
 
     func testIsStraight() {
         var hand: Hand
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("K♠︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("K♠︎")])
         XCTAssertTrue(hand.isStraight)
 
-        hand = Hand(cards: [fromString("2❤︎"), fromString("A♠︎")])
+        hand = Hand(cards: [cardFrom("2❤︎"), cardFrom("A♠︎")])
         XCTAssertTrue(hand.isStraight)
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("3♠︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("3♠︎")])
         XCTAssertFalse(hand.isStraight)
     }
 
     func testIsStraightFlush() {
         var hand: Hand
 
-        hand = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertTrue(hand.isStraightFlush)
 
-        hand = Hand(cards: [fromString("2❤︎"), fromString("A❤︎")])
+        hand = Hand(cards: [cardFrom("2❤︎"), cardFrom("A❤︎")])
         XCTAssertTrue(hand.isStraightFlush)
     }
 
@@ -128,12 +128,12 @@ final class PorkerTests: XCTestCase {
         var hand1: Hand
         var hand2: Hand
 
-        hand1 = Hand(cards: [fromString("A❤︎"), fromString("3❤︎")])
-        hand2 = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand1 = Hand(cards: [cardFrom("A❤︎"), cardFrom("3❤︎")])
+        hand2 = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertTrue(hand1 < hand2)
 
-        hand1 = Hand(cards: [fromString("A❤︎"), fromString("K♠︎")])
-        hand2 = Hand(cards: [fromString("A❤︎"), fromString("3❤︎")])
+        hand1 = Hand(cards: [cardFrom("A❤︎"), cardFrom("K♠︎")])
+        hand2 = Hand(cards: [cardFrom("A❤︎"), cardFrom("3❤︎")])
         XCTAssertTrue(hand1 < hand2)
     }
 
@@ -141,29 +141,29 @@ final class PorkerTests: XCTestCase {
         var hand1: Hand
         var hand2: Hand
 
-        hand1 = Hand(cards: [fromString("K♠︎"), fromString("Q♠︎")])
-        hand2 = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand1 = Hand(cards: [cardFrom("K♠︎"), cardFrom("Q♠︎")])
+        hand2 = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertTrue(hand1 < hand2)
 
-        hand1 = Hand(cards: [fromString("A♠︎"), fromString("2♠︎")])
-        hand2 = Hand(cards: [fromString("A❤︎"), fromString("K❤︎")])
+        hand1 = Hand(cards: [cardFrom("A♠︎"), cardFrom("2♠︎")])
+        hand2 = Hand(cards: [cardFrom("A❤︎"), cardFrom("K❤︎")])
         XCTAssertTrue(hand1 < hand2)
 
-        hand1 = Hand(cards: [fromString("A♠︎"), fromString("2♠︎")])
-        hand2 = Hand(cards: [fromString("4❤︎"), fromString("3❤︎")])
+        hand1 = Hand(cards: [cardFrom("A♠︎"), cardFrom("2♠︎")])
+        hand2 = Hand(cards: [cardFrom("4❤︎"), cardFrom("3❤︎")])
         XCTAssertTrue(hand1 < hand2)
     }
 
-    func fromString(_ str: String) -> Card {
+    func cardFrom(_ str: String) -> Card {
         let rank = String(str.prefix(str.count - 1))
         let suit = String(str.suffix(1))
         return Card(rank: Card.Rank.init(rawValue: rank)!, suit: Card.Suit.init(rawValue: suit)!)
     }
 
-    func handFromString(_ str: String) -> Hand {
+    func handFrom(_ str: String) -> Hand {
         // hands: card1 card2 ...
         let result = str.split(separator: ":")
-        let cards = result[1].split(separator: " ").map { fromString(String($0)) }
+        let cards = result[1].split(separator: " ").map { cardFrom(String($0)) }
         let hand = Hand(cards: cards)
         assert(hand.notation == str)
         return hand
